a71782b2e0fab55481ddf23fcfc69287325130b6
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-04-24 14:33:07 +0300

    modifies  src/components and added Links.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
+import About from "../components/About";
 
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+
 
 function App() {
+  const bio = 'I made this!';
+  const github = 'https://github.com/liza';
+  const linkedin = 'https://www.linkedin.com/in/liza/';
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home user={user} />
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Home(props) {
+  const { name, city, color, user } = props;
+
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color: user ? user.color : color }}>
+        {user ? `${user.name} is a Web Developer from ${user.city}` : `${name} is a Web Developer from ${city}`}
       </h1>
     </div>
   );
